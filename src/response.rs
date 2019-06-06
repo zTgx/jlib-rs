df4b681fe82c8eea32050bacdb93192a081990d3
beautifularea <beautifularea@gmail.com>
2019-06-06 17:59:15 +0800

    add response data structure

---
+++ src/response.rs
@@ -0,0 +1,19 @@
+//connection Response.
+/*
+*/
+struct Response {
+    fee_base: i32,
+    fee_ref: i32, 
+    hostid: String, 
+    ledger_hash: String,
+    ledger_index: i64,
+    ledger_time: i64, 
+    load_base: i32, 
+    load_factor: i32,
+    pubkey_node: String, 
+    random: String,
+    reserve_base: i32,
+    reserve_inc: i32,
+    server_status: String,
+    validated_ledgers: String, 
+}
