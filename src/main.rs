a749f5bb8ad689651f59e81c9e1b573a3cc25e11
zTgx <747674262@qq.com>
2019-06-07 18:51:05 +0800

    修改connect/request_server_info接口调用方式（后期可能还会修改）

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,53 @@ mod remote;
 mod server_info;
 use remote::Remote;
 
+extern crate ws;
+use ws::{connect, CloseCode, Message};
+
+static URL: &'static str = "ws://ts5.jingtum.com:5020";
+static URL2: &'static str = "ws://127.0.0.1:5060";
 fn main() {
     println!("Hello, world!");
 
-    let ret = Remote::new("ws://127.0.0.1:5060", false);
-    ret.connect();
-    //ret.request_server_info();
+    let ret = Remote::connect(|x| { match x {
+        Ok(x) => { 
+            println!("ret : {}", x.is_empty()); 
+            println!("len : {}", x.len());
+            if let Ok(x) = x.into_text() {
+                println!("text: {}", x);
+                        
+                use serde::{Serialize, Deserialize};
+                use serde_json::{Result, Value};
+                if let Ok(v) = serde_json::from_str(&x) as Result<Value> {
+                    println!("v : {}", v["ledger_hash"]);
+                }
+
+                //request server info
+                // ret.request_server_info();
+
+
+            }
+        }
+        Err(err) => { println!("error: {}", err); }
+    } 
+    });
+
+    Remote::request_server_info(|x| match x {
+        Ok(x) => {
+            println!("request info : {}", x);
+        }
+
+        Err(_) => {
+            
+        }
+    });
+
+    fn call_with_one<F>(func: F) -> usize
+    where F: Fn(usize) -> usize {
+    func(1)
+    }
+
+    let double = |x| x * 2;
+    assert_eq!(call_with_one(double), 2);
+
 }
--- src/remote.rs
+++ src/remote.rs
@@ -7,49 +7,118 @@ use websocket::result::WebSocketError;
 use websocket::{ClientBuilder, OwnedMessage};
 
 use crate::server_info::ServerInfo;
+use std::rc::Rc;
+    use std::any::Any;
+use std::cell::Cell;
+
+pub struct Conn {
+    conn: Option<Rc<ws::Sender>>,
+}
+
+impl Conn {
+    pub fn new(out: Option<Rc<ws::Sender>>) -> Self {
+        Conn {
+            conn: out,
+        }
+    }
+    pub fn request_server_info(&self) {
+        use serde_json::json;
+        let json = json!({ "id": "1", "command": "server_info" });
+        let compact = format!("{}", json);
+        println!("compact : {}", compact);
+   
+    }
+}
+
+pub struct Config {
+    addr: str,
+}
+impl Config {
+    pub fn get_url() -> &'static str {
+        "ws://ts5.jingtum.com:5020"
+    }
+}
+
+
+#[derive(Clone)]
+pub struct Dum {
+    pub text: String,
+}
+impl Dum {
+    pub fn new(s: String) -> Self {
+        Dum { text: s, }
+    }
+}
 
 pub struct Remote {
     addr: &'static str,
     local_sign: bool,
+    conn: Option<Conn>,
 }
 
-impl Remote {
+impl Remote  {
     pub fn new(addr: &'static str, local_sign: bool) -> Self {
         Remote {
             addr: addr,
             local_sign: local_sign,
+            conn: None,
         }
     }
 
-    pub fn connect(&self) {
+    pub fn connect<F>(op: F) 
+        where F: Fn(Result<ws::Message, &'static str>) {
         extern crate ws;
-
         use ws::{connect, CloseCode};
 
-        connect(self.addr, |out| {
+        connect(Config::get_url(), |out| {
+            
+            let dumx = Rc::new(out);
+            let out = dumx.clone();
+            //self.f = None;
+
 
             use serde_json::json;
             let json = json!({ "id": "0", "command": "subscribe" , "streams" : ["ledger","server","transactions"]});
             let compact = format!("{}", json);
             println!("input : {}", compact);
             out.send(compact).unwrap();
+
             move |msg| {
-
                 println!("get : {}", msg);
-                let json = json!({ "id": "1", "command": "server_info" });
-                let compact = format!("{}", json);
-                println!("compact : {}", compact);
-                out.send(compact).unwrap();
+                //op(Ok(msg));
 
-                // move |x| {
-                //     println!("x : {}", x);
-                    
-                //     out.close(CloseCode::Normal)
-                // };
-
+                //request_server_info();
                 out.close(CloseCode::Normal)
-                
+
+                //Ok(())
             }
+
+
+            
+
+            //             let mut conn_real = Conn::new(Some(dumx.clone()));
+            // conn_real.request_server_info();
+
+            // Ok(())
+            // move |x| {
+            //     println!("get : {}", x);
+            //     let json = json!({ "id": "1", "command": "server_info" });
+            //     let compact = format!("{}", json);
+            //     println!("compact : {}", compact);
+            //     //op(Ok("xxxxllll".to_string()));
+
+            //     //out.send(compact).unwrap();
+
+            //     // move |x| {
+            //     //     println!("x : {}", x);
+                    
+            //     //     out.close(CloseCode::Normal)
+            //     // };
+
+                
+
+            //     out.close(CloseCode::Normal)                
+            // }
         }).unwrap()
     } 
 
@@ -61,8 +130,62 @@ impl Remote {
         true
     }
 
+
+    pub fn print_if(value: Rc<dyn Any>) -> String {
+        println!("print--------------------");
+        match value.downcast::<Cell<String>>() {
+            Ok(string) => {
+                return string.take();
+            },
+
+            _ => { "".to_string() }
+
+        // return string.take();
+    }
+    }
+
     //get 
-    pub fn request_server_info(&self) -> Box<ServerInfo> {
+    pub fn request_server_info<F> (op: F) -> Box<ServerInfo> 
+        where F: Fn(Result<String, &'static str>) {
+
+        extern crate ws;
+        use ws::{connect, CloseCode};
+        
+        let  info = Rc::new(Cell::new("".to_string()));
+
+let total = Rc::new(Cell::new(Dum::new("".to_string())));
+
+        connect(Config::get_url(), |out| { 
+        let copy = info.clone();
+        let my_total = total.clone();
+
+        use serde_json::json;
+        let json = json!({ "id": "1", "command": "server_info" });
+        let compact = format!("{}", json);
+        println!("compact : {}", compact);
+
+        out.send(compact).unwrap();
+
+        move |msg: ws::Message| {
+            println!("request server info : {}", msg);
+            //op(Ok(msg));
+
+            //request_server_info();
+            let c = msg.as_text()?;
+            println!("c: {}", c);
+            copy.set(c.to_string());
+            
+            let nedu = Dum::new("cddddddegrgrgfdgdfdfdfdfdfdfd".to_string());
+my_total.set(nedu);
+
+            out.close(CloseCode::Normal) 
+        }
+        
+        }).unwrap();
+        
+        let re = Remote::print_if(info);
+        println!("re : {}", re);
+        op(Ok(re));
 
         Box::new( ServerInfo {
                                     ledger: String::from(""),
