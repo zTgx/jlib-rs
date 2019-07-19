3a85fd5a37b40fa105cf8e7e3804916c8f7bc820
beautifularea <beautifularea@gmail.com>
2019-07-19 15:19:14 +0800

    fix example config

--- examples/t04_cancel_offer.rs
+++ examples/t04_cancel_offer.rs
@@ -5,7 +5,7 @@ use jlib::api::transaction::cancel_offer::*;
 use jlib::message::transaction::offer_cancel::{OfferCancelTxResponse, OfferCancelSideKick};
 
 fn main() {
-    let config = Config::default_with_box();
+    let config = Config::new(TEST1, true);
 
     let account: String = "jB7rxgh43ncbTX4WeMoeadiGMfmfqY2xLZ".to_string();
     let secret:String= "sn37nYrQ6KPJvTFmaBYokS3FjXUWd".to_string();
