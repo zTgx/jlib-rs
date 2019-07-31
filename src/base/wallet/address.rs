64c067d6eadab6fd6a94afac393f6cf640342368
zTgx <747674262@qq.com>
2019-07-31 13:43:03 +0800

    add generate_str to create secret && address

--- src/base/wallet/address.rs
+++ src/base/wallet/address.rs
@@ -1,13 +1,11 @@
 
 use crate::base::ring::{digest};
 
-// use crate::base::crypto::ripemd160::Ripemd160 ;
-// use crate::base::crypto::digest::Digest;
-
 use crate::base::curve::ripemd160::JRipemd160;
-
-use crate::base::misc::util::*;
 use crate::base::wallet::keypair::*;
+use crate::base::wallet::generate_str;
+
+static H_ADDRESS: &[u8] = &[0];
 
 #[derive(Debug)]
 pub struct WalletAddress {}
@@ -33,42 +31,19 @@ impl <'a> WalletAddressBuilder <'a> {
     }
 
     pub fn generate(&self) -> String {
-        let mut key: Vec<u8> = vec![0];
-        if let Ok(x) = hex::decode(&self.key_pair.public_key) {
-            key = x;
-        }
+        let key: Vec<u8> = hex::decode(&self.key_pair.public_key).unwrap();
         let mut ctx = digest::Context::new(&digest::SHA256);
         ctx.update(&key);
-        let mut key = [0u8; 32];
-        key.copy_from_slice(ctx.finish().as_ref());
 
-        let input = key;
+        let mut input = [0u8; 32];
+        input.copy_from_slice(ctx.finish().as_ref());
 
         let mut ripemd160 = JRipemd160::new();
         ripemd160.input(&input);
         let ret: &mut [u8] = &mut [0u8;20];
         ripemd160.result(ret);
-        let ripemd160x= ripemd160.result_str();
 
-        let mut xy = "".to_string();
-        if let Ok(_args) = hex::decode(ripemd160x) {
-
-            let mut version: Vec<u8> = [0].to_vec();
-
-            //4. concat args
-            concat_args(&mut version, &ret.to_vec());
-
-            //5. encodechecked.
-            let checked: Vec<u8> = encode_checked(&mut version);
-
-            //6. concat args
-            concat_args(&mut version, &checked);
-
-            let address = encode_raw(&mut version);
-
-            xy = address;
-        }
-
-        xy
+        let mut version: Vec<u8> = H_ADDRESS.to_vec();
+        generate_str(&mut version, &ret.to_vec())
     }
 }
--- src/base/wallet/mod.rs
+++ src/base/wallet/mod.rs
@@ -3,3 +3,34 @@ pub mod config;
 pub mod keypair;
 pub mod address;
 pub mod seed;
+
+use basex_rs::BaseX;
+use crate::base::curve::sha256::JSha256;
+
+//generate seed && address
+pub fn generate_str(version: &mut Vec<u8>, so: &Vec<u8>) -> String {
+    //4. concat args
+    version.extend(so);
+
+    //5. sha256
+    let checked: Vec<u8> = JSha256::sha256(&version);
+
+    //6. take 0..4
+    let token = checked.get(..4).unwrap().to_vec();
+
+    //7. concat args
+    version.extend(token);
+
+    //end. base58 encode
+    BaseX::encode(version.as_mut_slice())
+}
+
+pub fn check_address(address: &String) -> bool {
+
+    true
+}
+
+pub fn check_secret(secret: &String) -> bool {
+
+    true
+}
