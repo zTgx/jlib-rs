de0e7bd1870380668583f09b8a691120b826b5c2
zTgx <747674262@qq.com>
2019-10-28 17:27:03 +0800

    update example / n08_account_tx

--- examples/n08_account_tx.rs
+++ examples/n08_account_tx.rs
@@ -7,7 +7,7 @@ use jlib::api::query::account_tx::*;
 use jlib::message::query::account_tx::{RequestAccountTxResponse, AccounTxSideKick};
 
 fn main() {
-    let config = Config::new(TEST1, true);
+    let config = Config::new(TEST3, true);
     let account = "jHb9CJAWyB4jr91VRWn96DkukG4bwdtyTh".to_string();
     AccountTx::new().request_account_tx(config.clone(), account, Some(1), |x| match x {
         Ok(response) => {
