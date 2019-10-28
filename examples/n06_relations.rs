cf36764f0eab270f5835d354390b6c785d7e4875
zTgx <747674262@qq.com>
2019-10-28 17:23:53 +0800

    update example / n06_relations

--- examples/n06_relations.rs
+++ examples/n06_relations.rs
@@ -5,7 +5,7 @@ use jlib::api::query::relations::*;
 use jlib::message::query::relations::{RequestAccountRelationsResponse, RelationsSideKick};
 
 fn main() {
-    let config = Config::new(TEST1, true);
+    let config = Config::new(TEST3, true);
     let account = "jB7rxgh43ncbTX4WeMoeadiGMfmfqY2xLZ".to_string();
     let rtype = Some("trust".to_string());
     Relations::new().request_account_relations(config.clone(), account, rtype, |x| match x {
