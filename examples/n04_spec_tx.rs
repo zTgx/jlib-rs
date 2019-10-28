cb513316a8fb0c384fa4db302cb98cdca9392551
zTgx <747674262@qq.com>
2019-10-28 17:19:08 +0800

    update example / n04_spec_tx

--- examples/n04_spec_tx.rs
+++ examples/n04_spec_tx.rs
@@ -5,7 +5,7 @@ use jlib::api::query::spec_tx::*;
 use jlib::message::query::spec_tx::{RequestTxResponse, SpecTxSideKick};
 
 fn main() {
-    let config = Config::new(TEST1, true);
+    let config = Config::new(TEST3, true);
     let tx_hash = "4552D9C58078855888A966F4FEE4FA46C413211A96C3174A7980651106C4E2DA".to_string();
     SpecTx::new().request_tx(config.clone(), tx_hash, |x| match x {
         Ok(response) => {
