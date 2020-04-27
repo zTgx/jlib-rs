19e5860c80768d5a298c199dad45aa726cfe82d5
zTgx <747674262@qq.com>
2020-04-27 14:27:20 +0800

    update example n03_account_info

--- examples/n03_account_info.rs
+++ examples/n03_account_info.rs
@@ -4,30 +4,18 @@ use jlib::misc::config::*;
 use jlib::api::query::account_info::*;
 use jlib::message::query::account_info::{RequestAccountInfoResponse, AccounInfoSideKick};
 
-use std::rc::Rc;
-use std::cell::Cell;
-use jlib::base::misc::util::{downcast_to_usize};
-
 fn main() {
     let config = Config::new(TEST3, true);
     let account = "jB7rxgh43ncbTX4WeMoeadiGMfmfqY2xLZ".to_string();
 
-    let seq_rc = Rc::new(Cell::new(0u64));
     AccountInfo::new().request_account_info(config.clone(), account, |x| match x {
         Ok(response) => {
             let res: RequestAccountInfoResponse = response;
-            println!("账号信息: \n{:?}", &res);
-
-            let seq = seq_rc.clone();
-            seq.set(res.sequence);
+            println!("account info: \n{:?}", &res);
         },
-
         Err(e) => {
             let err: AccounInfoSideKick = e;
             println!("{:?}", err);
         }
     });
-
-    let x = downcast_to_usize(seq_rc);
-    println!("x : {}", x);
 }
