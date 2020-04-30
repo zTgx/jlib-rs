8e858b7533088e5ac3be1771f50912e69a744ef9
zTgx <747674262@qq.com>
2020-04-30 11:27:53 +0800

    lib.rs add Copyright

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
+// Copyright 2019-2020 zTgx <beautifularea@163.com>.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 #[macro_use]
 extern crate lazy_static;
-
 extern crate typename;
 extern crate rand;
 extern crate serde_json;
@@ -11,6 +18,7 @@ extern crate ws;
 extern crate basex_rs;
 extern crate cast_rs;
 
+// exports
 pub mod base;
 pub mod message;
 pub mod misc;
@@ -28,3 +36,5 @@ pub enum WalletType {
 pub fn generate_wallet(wtype: WalletType) -> Wallet {
     Wallet::new(wtype)
 }
+
+
