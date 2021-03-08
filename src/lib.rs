1860ccf73cd80d6301b4aa2fcc05d42ed0b93b41
zTgx <747674262@qq.com>
2021-03-08 21:32:31 +0800

    2021年了。

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2019-2020 zTgx <beautifularea@163.com>.
+// Copyright 2019-2021 zTgx <beautifularea@163.com>.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
