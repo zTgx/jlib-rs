396eeddb75222be9472e6c828605a142f6f27798
zTgx <747674262@qq.com>
2021-03-23 21:04:54 +0800

    1、hash不一致，是因为在使用ipfs-unixfs的情况下，多sha2_256了一次。 2、调整only_hash代码。

--- examples/r00_ipfs.rs
+++ examples/r00_ipfs.rs
@@ -1,19 +1,17 @@
 extern crate jlib;
 
 use jlib::api::ipfs::hash::only_hash;
-
-use std::fs::File;
  
 fn main() {
     /*
-    hello world -> Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD
+        hello world -> Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD
+        1220F852C7FA62F971817F54D8A80DCD63FCF7098B3CBDE9AE8EC1EE449013EC5DB0
     */
     let target = "Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD".to_owned();
     let digest = "hello world".as_bytes().to_vec();
     if let Some(hash) = only_hash( &digest ) {
-        println!("ipfs hash: {}", hash);
         assert_eq!(hash, target);
     } else {
         println!("ipfs hash none");
     }
-}
+} 
--- src/api/ipfs/hash.rs
+++ src/api/ipfs/hash.rs
@@ -1,23 +1,22 @@
 
-use basex_rs::{BaseX, Decode, BITCOIN, Encode};
 use ipfs_unixfs::file::adder::FileAdder;
-use multihash::{Code};
-use multihash::MultihashDigest;
+use basex_rs::{BaseX, BITCOIN, Encode}; 
 
+/*
+    【输入】 digest 需要计算hash的文件字节流
+    【输出】 Option类型、计算后的hash
+*/
 pub fn only_hash(digest : &Vec<u8>) -> Option<String> { 
     let mut file_addr = FileAdder::builder().build();
-
-    let hash = Code::Sha2_256.digest( &digest );
-    file_addr.push( &hash.to_bytes() );
-
+    file_addr.push( &digest );
     let mut finish = file_addr.finish();
 
     if let Some( (cid, _block) ) = finish.next() {
         let data = cid.to_bytes();
-        let hash = cid.hash();
-        println!("data: {:#?}", cid);
 
-        return Some( BaseX::new(BITCOIN).encode(&data) );
+        let hash = BaseX::new(BITCOIN).encode( &data );
+
+        return Some( hash );
     }
 
     None
