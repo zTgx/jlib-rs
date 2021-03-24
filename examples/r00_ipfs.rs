ed563171d6388c548d5727d26264eb16c980847d
zTgx <747674262@qq.com>
2021-03-24 16:37:20 +0800

    1、解决上次遗留问题，（大文件hash不一致）。默认 push大小是 256 * 1024,最后返回root hash。 具体实现可以参考 unixfs的 push / finish方法。 2、更新example / ipfs。

--- examples/r00_ipfs.rs
+++ examples/r00_ipfs.rs
@@ -5,16 +5,55 @@ use jlib::api::ipfs::hash::only_hash;
 use std::fs::File;
 use std::io::prelude::*;
 
-
-fn main() {
+fn big_file_hash() {
+    // let mut f = File::open("/mnt/c/Users/zTgx/Desktop/github.com.c/ipfs/1.png").unwrap();
     let mut f = File::open("/mnt/c/Users/zTgx/Desktop/github.com.c/ipfs/1.jpg").unwrap();
     let mut buf = Vec::new();
-    f.read(&mut buf).unwrap();
-    
-    // target: QmdSNzh5fsfUTz66adXkDFVFFmDGpPSvziEp7u8bZcxYAr
+    f.read_to_end(&mut buf).unwrap();
+    println!("buf size: {:?}", buf.len());
+
+    let target = "QmdSNzh5fsfUTz66adXkDFVFFmDGpPSvziEp7u8bZcxYAr".to_owned();
     if let Some(hash) = only_hash( &buf ) {
-        println!("hash : {}", hash);
+        println!("big file hash: {}", hash);
+        assert_eq!(hash, target);
     } else {
         println!("ipfs hash none");
+    }
+}
+
+fn little_file_hash() {
+    let target = "Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD".to_owned();
+    let digest = "hello world".as_bytes().to_vec();
+    let hash = only_hash( &digest ).unwrap();
+    assert_eq!(hash, target);
+}
+
+enum Case {
+    NoneFile,
+    BigFile,
+    LittleFile,
+}
+
+// ---------------------------------------------------------------------------------------------------------
+//
+// ipfs 主方法
+//
+// ---------------------------------------------------------------------------------------------------------
+fn main() {
+    let case = Case::BigFile;
+    let case = Case::LittleFile;
+
+    match case {
+        Case::NoneFile => {
+
+        },
+
+        Case::BigFile => {
+            big_file_hash();
+        },
+
+        Case::LittleFile => {
+            little_file_hash()
+        }
     }
 } 
--- src/api/ipfs/hash.rs
+++ src/api/ipfs/hash.rs
@@ -2,19 +2,126 @@
 use ipfs_unixfs::file::adder::FileAdder;
 use basex_rs::{BaseX, BITCOIN, Encode}; 
 
+use ipfs_unixfs::file::adder::Chunker;
+use ipfs_unixfs::file::adder::BalancedCollector;
+
 /*
     【输入】 digest 需要计算hash的文件字节流
     【输出】 Option类型、计算后的hash
 */
 pub fn only_hash(digest : &Vec<u8>) -> Option<String> { 
+    // let mut file_addr = FileAdder::builder().build();
+    // file_addr.push( &digest );
+
+    // println!("finish: {:#?}", file_addr);
+
+    // let mut finish = file_addr.finish();
+
+
+    // if let Some( (cid, _block) ) = finish.next() {
+    //     let data = cid.to_bytes();
+
+    //     let hash = BaseX::new(BITCOIN).encode( &data );
+
+    //     return Some( hash );
+    // }
+
+    // None
+
+    // only_hash_chunk(&digest)
+
+    // only_hash_v3(&digest)
+
+    only_hash_v4(&digest)
+}
+
+pub fn only_hash_chunk(digest: &Vec<u8>) -> Option<String> {
     let mut file_addr = FileAdder::builder().build();
     file_addr.push( &digest );
+
+    println!("finish: {:#?}", file_addr);
+
     let mut finish = file_addr.finish();
 
-    if let Some( (cid, _block) ) = finish.next() {
+
+    let cid = finish.next().unwrap().0;
+    println!("cid: {:?}", cid);
+     {
         let data = cid.to_bytes();
 
         let hash = BaseX::new(BITCOIN).encode( &data );
+
+        return Some( hash );
+    }
+
+    None
+}
+
+pub fn only_hash_v3(digest: &Vec<u8>) -> Option<String> {
+    let mut adder = FileAdder::default();
+    let mut last_block = String::new();
+    // println!("digest.len: {}", digest.len());
+
+    let mut total = 0;
+    loop {
+        if total >= digest.len() {
+            // eprintln!("finishing");
+            // println!("adder: {:#?}", adder);
+
+            let mut blocks = adder.finish();
+
+            let next = blocks.next();
+
+            let item = next.unwrap().0;
+            // println!("cid : {:?}", item);
+
+            let hash = BaseX::new(BITCOIN).encode( &item.to_bytes() );
+            // println!("hash: xxx {:?}", hash);
+            last_block = hash;
+
+            break;
+        }
+
+        while total < digest.len() {
+            let (blocks, consumed) = adder.push(&digest[total..]);
+            total += consumed;
+
+            // println!("totoal: {}", total);
+            // println!("consumed: {}", consumed);
+        }
+    }
+
+    Some( last_block )
+}
+
+pub fn only_hash_v4(digest: &Vec<u8>) -> Option<String> {
+    let mut adder = FileAdder::default();
+
+    let mut amt = 0;
+    let mut written = 0;
+    let mut blocks_received = Vec::new();
+
+    if amt == 0 {
+        amt = digest.len();
+    }
+
+    while written < digest.len() {
+        let end = written + (digest.len() - written).min(amt);
+        let slice = &digest[written..end];
+
+        let (blocks, pushed) = adder.push(slice);
+        blocks_received.extend(blocks);
+        written += pushed;
+    }
+
+    let last_blocks = adder.finish();
+
+    blocks_received.extend(last_blocks);
+
+    let blocks_len = blocks_received.len();
+    if blocks_len > 0 {
+        let last = &blocks_received[blocks_len - 1].0.to_bytes();
+        let hash = BaseX::new(BITCOIN).encode( &last );
 
         return Some( hash );
     }
