67c369789806c25aa75d30d11d9b2ae8a47c13e1
zTgx <747674262@qq.com>
2021-03-24 18:40:11 +0800

    更新 基础配置文件中的国密算法零号、一号地址。

--- src/base/base_config.rs
+++ src/base/base_config.rs
@@ -1,4 +1,13 @@
-pub const CURRENCY     : &'static str = "SWT";
+// 基础币
+pub const CURRENCY     : &'static str = "SWT"; 
+
+//手续费
+pub const FEE          : u64 = 10000; 
+
+//SECP256K1 加密算法对应的零号、一号地址
 pub const ACCOUNT_ZERO : &'static str = "jjjjjjjjjjjjjjjjjjjjjhoLvTp";
 pub const ACCOUNT_ONE  : &'static str = "jjjjjjjjjjjjjjjjjjjjBZbvri";
-pub const FEE          : u64 = 10000;
+
+//SM2P256V1 加密算法对应的零号、一号地址
+pub const ACCOUNT_ZERO_SM2P256V1 : &'static str = "jjjjjjjjjjjjjjjjjjjjjn1TT5q"; //swt的issuer
+pub const ACCOUNT_ONE_SM2P256V1  : &'static str = "jjjjjjjjjjjjjjjjjjjjwVBfmE";  //占位地址
